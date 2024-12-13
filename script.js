a1fce774a43948a6defb6267d3f84b3fb39ef93e
DillaKanitha <soejonosoesilo@gmail.com>
2024-12-14 00:53:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-  $('.title').click(function(){
-    $('.container').addClass('open');
-  });
-  
-  
-  $('.close').click(function(){
-    $('.container').removeClass('open');
+$(document).ready(function () {
+  $('.ico').click(function () {
+    const $heart = $(this);
+
+    // Add the scaling animation
+    $heart.addClass('open');
+
+    // Ensure the heart disappears after the animation
+    setTimeout(() => {
+      $heart.remove(); // Remove the heart completely
+      $('.images').removeClass('d-none'); // Show images
+    }, 800); // Matches the animation duration
   });
 });
